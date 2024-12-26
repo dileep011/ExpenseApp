196e1922459597d7422662e746f5252faff5478a
Reddi DIleepKumar <78788832+dileep011@users.noreply.github.com>
2024-12-26 16:01:05 +0530

    Add files via upload

---
+++ Repository/UserdetailsRepository.cs
@@ -0,0 +1,38 @@
+﻿using ExpenseApp.Models.Entity;
+
+namespace ExpenseApp.Repository
+{
+    public class UserdetailsRepository : IUserRepository
+    {
+        private readonly
+        UserDetails IUserRepository.Delete(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<UserDetails> IUserRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDetails IUserRepository.GetById(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDetails IUserRepository.Login(int UserId, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDetails IUserRepository.Register(UserDetails userDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        UserDetails IUserRepository.Update(UserDetails userDetails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
